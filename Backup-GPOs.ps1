62914bfbebee65a27c54a3cc4ca9108b78134520
Ben Gardner <ben.gardner@nwmotorsport.com>
2019-08-08 09:25:48 -0700

    Last run logged, looks for that date first

--- Backup-GPOs.ps1
+++ Backup-GPOs.ps1
@@ -2,15 +2,30 @@
 .SYNOPSIS
     Backup GPOs by running Export-GPO and using git to backup the folder
 .DESCRIPTION
+    Look for last run log and import date, otherwise date is -24hrs
     Run Export-GPOs
     Run Git to commit and push
+    Record run time
 .NOTES
      Author     : Benjamin Gardner bgardner160@gmail.com
 #>
 
 . \\internal.northwestmotorsportinc.com\resources\scripts\Modules\Export-GPOs.ps1
 
-Export-GPOs
+#Look for last run log and import date, otherwise date is -24hrs
+Try {
+    $Date = [datetime]$(Get-Content -Path "\\contoso\Public\Internet Department\gpo\lastrun.log")
+}
+Catch {
+    $Date = $((Get-Date).AddDays(-1))
+}
 
+#Export GPOs in the last 24 hrs.
+Export-GPOs -Date $Date
+
+#Record run date
+$lastRun = Out-File -FilePath "\\contoso\Public\Internet Department\gpo\lastrun.log" -InputObject "$(Get-Date)" -Force
+
+#This requires git to be configured on the computer with correct permissons and \\contoso\Public to be assigned S Drive.
 &  "C:\Program Files\Git\bin\bash.exe" --login -i -c "cd 'S:\Internet Department\gpo' && git add . && git commit -m 'daily backup'"
 &  "C:\Program Files\Git\bin\bash.exe" --login -i -c "cd 'S:\Internet Department\gpo' && git push"
