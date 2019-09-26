c478cbadfb2045fe0b2cb8aa4b7f8e23b687b4da
Ben Gardner <ben.gardner@nwmotorsport.com>
2019-09-26 12:25:35 -0700

    Get and Set hidden mail users

---
+++ Modules/Get-ADUsersHidden/Get-ADUsersHidden.psm1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+    Gets all AD users with hide flag
+.DESCRIPTION
+    Parameters
+    Get-AD Users
+        filter by enabled 
+        with all properties 
+        in searchbase
+        where hide is true
+.Parameter SearchBase
+    OU to search, default 'OU=Users,DC=internal,DC=contoso,DC=com'
+.Parameter Enabled
+    String! requires '', If AD users to search are enabled or not, default = '$true'
+.NOTES
+        Author     : Benjamin Gardner bgardner160@gmail.com
+#>
+
+
+Function Get-ADUsersHidden {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+    [Parameter()][string]$SearchBase = 'OU=Users,DC=internal,DC=contoso,DC=com',
+    [Parameter()][string][ValidateSet('$true','$false')]$Enabled = '$true'
+    )
+
+    $Users = Get-AdUser -Filter "enabled -eq $Enabled" -Properties * -SearchBase $SearchBase | Where-object {$_.msExchHideFromAddressLists -eq 'TRUE'}
+
+    Return $Users
+}
+
+Export-ModuleMember -Function Get-ADUsersHidden
---
+++ Modules/Set-ADUsersHidden/Set-ADUsersHidden.psm1
@@ -0,0 +1,46 @@
+﻿<#
+.SYNOPSIS
+    Sets AD objects to hidden from address lists, can unhide
+.DESCRIPTION
+    Parameters
+    If unhide
+        foreach user
+            set to unhide
+    else
+        foreach user
+            try to add hide as true
+            catch to replace hide as true
+.Parameter Users
+    AD User Objects to affect
+.Parameter Unhide
+    To unhide users, boolean value, default = $False
+.NOTES
+        Author     : Benjamin Gardner bgardner160@gmail.com
+#>
+
+
+Function Set-ADUsersHidden {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+    [Parameter(ValueFromPipeline)][Microsoft.ActiveDirectory.Management.ADUser]$User,
+    [Parameter()][boolean]$Unhide = $False
+    )
+process {
+    if($Unhide){
+            Write-Verbose "$User.Name replacing False to HideFromAddressLists"
+            Set-ADuser -Identity $User.ObjectGUID.Guid -Replace @{msExchHideFromAddressLists="FALSE"} -Verbose
+        }
+    else {
+            Try {
+                Write-Verbose "$User.Name adding True to HideFromAddressLists"
+                Set-ADuser -Identity $User.ObjectGUID.Guid -Add @{msExchHideFromAddressLists="TRUE"} -Verbose
+            }
+            Catch{
+                Write-Verbose "$User.Name replacing True to HideFromAddressLists"
+                Set-ADuser -Identity $User.ObjectGUID.Guid -Replace @{msExchHideFromAddressLists="TRUE"} -Verbose
+            }
+        }
+    }
+}
+
+Export-ModuleMember -Function Set-ADUsersHidden
