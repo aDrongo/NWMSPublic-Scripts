100b82bb5285c375da18e30b717cd3bbcf042db1
Ben Gardner <ben.gardner@nwmotorsport.com>
2019-10-23 12:09:53 -0700

    Added Azure license support

---
+++ Modules/Get-AzureSkuCount/Get-AzureSkuCount.psm1
@@ -0,0 +1,12 @@
+﻿function Get-AzureSkuCount {    #Need to install Install-Module -Name AzureAD
+    Try{ Import-Module AzureAD -Force } Catch { Install-PackageProvider nuget -force; Install-Module AzureAD -Scope CurrentUser -Force -Confirm:$false -ErrorAction SilentlyContinue -AllowClobber }
+    $password = "PASSWORD" | ConvertTo-SecureString -asPlainText -Force    $username = "USERNAME@CONTOSO.COM"
+    $credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+    
+
+    Connect-AzureAD -Credential $credential | Out-Null
+
+    $licenseMap = Get-AzureADSubscribedSku | Select ConsumedUnits,PrepaidUnits,ObjectId,SkuId,SkuPartNumber | where {$_.SkuPartNumber -match "O365_BUSINESS_ESSENTIALS|ENTERPRISEPACK"}
+    Return $licenseMap
+}
--- Report-ADUsers.ps1
+++ Report-ADUsers.ps1
@@ -1,9 +1,10 @@
-﻿#Weekly Report that get's AD User stats, hidden users, disabled but not moved users, oldest passwords, oldest logins, locked out accounts, accounts with no password expiring and admin accounts. Converts to HTML and emails report, schedule to run weekly.
+﻿#Weekly Report
 
 Import-Module ActiveDirectory
-Import-Module \\internal.contoso.com\resources\scripts\Modules\Get-ADUsersHidden
-Import-Module \\internal.contoso.com\resources\scripts\Modules\ConvertHtml
-Import-Module \\internal.contoso.com\resources\scripts\Private\Send-Mailcontoso
+Import-Module \\internal.contosoinc.com\resources\scripts\Modules\Get-ADUsersHidden
+Import-Module \\internal.contosoinc.com\resources\scripts\Modules\ConvertHtml
+Import-Module \\internal.contosoinc.com\resources\scripts\Modules\Private\Send-Mailnwms
+Import-Module \\internal.contosoinc.com\resources\scripts\Modules\Execute-Command
 
 $convertParams = @{ 
  head = @"
@@ -19,38 +20,63 @@ div { word-wrap: break-word;}
 "@
 }
 
-$UserEnabledCount = $(Get-ADUser -Filter "Enabled -eq '$true'" -SearchBase "OU=contoso Users,DC=internal,DC=contoso,DC=com").Count
-$UserDisabledCount = $(Get-ADUser -Filter "Enabled -eq '$false'" -SearchBase "OU=contoso Users,DC=internal,DC=contoso,DC=com").Count
+$UserEnabledCount = $(Get-ADUser -Filter "Enabled -eq '$true'" -SearchBase "OU=NWMS Users,DC=internal,DC=contosoinc,DC=com").Count
+$UserDisabledCount = $(Get-ADUser -Filter "Enabled -eq '$false'" -SearchBase "OU=NWMS Users,DC=internal,DC=contosoinc,DC=com").Count
 $ServiceEnabledCount = $(Get-ADUser -Filter "Enabled -eq '$true'").Count - $UserEnabledCount
 $UserCount = New-Object PSObject
 $UserCount | Add-Member -MemberType NoteProperty -Name 'Users Enabled' -Value $UserEnabledCount
 $UserCount | Add-Member -MemberType NoteProperty -Name 'Users Disabled' -Value $UserDisabledCount
 $UserCount | Add-Member -MemberType NoteProperty -Name 'Services Enabled' -Value $ServiceEnabledCount
 
+#Get Azure Licensing Sku Counts
+Try{
+    #Need to run this command as 64bit(jenkins runs as 32bit) so call external 64bit powershell and return output
+    $EnterpriseSkuCount = Execute-Command -commandTitle "Get-AzureSkuCount" -commandPath "$($env:SystemRoot)\sysnative\WindowsPowerShell\v1.0\powershell.exe" -commandArguments '& {Import-Module \\internal.contosoinc.com\resources\scripts\Modules\Private\Get-AzureSkuCount; $AzureCount = Get-AzureSkuCount; Return $AzureCount | Where SkuPartNumber -match "ENTERPRISEPACK"}'
+    $EnterpriseSkuCount = $EnterpriseSkuCount.stdout
+
+    $BusinessSkuCount = Execute-Command -commandTitle "Get-AzureSkuCount" -commandPath "$($env:SystemRoot)\sysnative\WindowsPowerShell\v1.0\powershell.exe" -commandArguments '& {Import-Module \\internal.contosoinc.com\resources\scripts\Modules\Private\Get-AzureSkuCount; $AzureCount = Get-AzureSkuCount; Return $AzureCount | Where SkuPartNumber -match "O365_BUSINESS_ESSENTIALS"}'
+    $BusinessSkuCount = $BusinessSkuCount.stdout
+
+    #Output is a string so need to parse it
+    $EnterpriseSkuCount = $EnterpriseSkuCount.Split([Environment]::NewLine)
+    $EnterpriseConsumed = $($EnterpriseSkuCount | Select-String -Pattern "ConsumedUnits").ToString() -replace '[\D]*',""
+    $EnterpriseEnabled = $($EnterpriseSkuCount | Select-String -Pattern "Enabled").ToString() -replace '[\D]*',""
+
+    $BusinessSkuCount = $BusinessSkuCount.Split([Environment]::NewLine)
+    $BusinessConsumed = $($BusinessSkuCount | Select-String -Pattern "ConsumedUnits").ToString() -replace '[\D]*',""
+    $BusinessEnabled = $($BusinessSkuCount | Select-String -Pattern "Enabled").ToString() -replace '[\D]*',""
+
+    $UserCount | Add-Member -MemberType NoteProperty -Name "E3 Enterprise" -Value "$($EnterpriseConsumed)/$($EnterpriseEnabled)"
+    $UserCount | Add-Member -MemberType NoteProperty -Name "Business Essentials" -Value "$($BusinessConsumed)/$($BusinessEnabled)"
+}
+Catch{
+    $UserCount | Add-Member -MemberType NoteProperty -Name 'Azure License Count' -Value "Error getting License Count"
+}
+
 #Hidden users
-$ADUsersHidden = Get-ADUsersHidden
+$ADUsersHidden = Get-ADUsersHidden | Select Name,SamAccountName,Title,Department,LastLogonDate
 if(!$ADUsersHidden){$ADUsersHidden = "None"}
 
 #Non-Disabled Former Employees
-$NonDisabledFormer = Get-ADUser -Filter "Enabled -eq '$true'" -SearchBase "OU=Former Employees,OU=contoso Users,DC=internal,DC=contoso,DC=com"
+$NonDisabledFormer = Get-ADUser -Filter "Enabled -eq '$true'" -SearchBase "OU=Former Employees,OU=NWMS Users,DC=internal,DC=contosoinc,DC=com" | Select Name,SamAccountName,Title,Department,LastLogonDate
 if(!$NonDisabledFormer){$NonDisabledFormer = 'None'}
 
 #Oldest Passwords
-$OldestPasswords = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "OU=contoso Users,DC=internal,DC=contoso,DC=com" -Properties PasswordLastSet | Sort-Object PasswordLastSet |where-object {$_.PasswordLastSet -ne $null} | select Name,SamAccountName,PasswordLastSet -First 10
+$OldestPasswords = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "OU=NWMS Users,DC=internal,DC=contosoinc,DC=com" -Properties PasswordLastSet,Title,Department | Sort-Object PasswordLastSet |where-object {$_.PasswordLastSet -ne $null} | select Name,SamAccountName,Title,Department,PasswordLastSet -First 10
 
 #LastLogonDate
-$LastLogonDate = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "OU=contoso Users,DC=internal,DC=contoso,DC=com" -Properties LastLogonDate |where-object {$_.LastLogonDate -ne $null} | Sort-Object LastLogonDate | Select Name,SamAccountName,LastLogonDate -First 10
+$LastLogonDate = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "OU=NWMS Users,DC=internal,DC=contosoinc,DC=com" -Properties LastLogonDate,Title,Department |where-object {$_.LastLogonDate -ne $null} | Sort-Object LastLogonDate | Select Name,SamAccountName,Title,Department,LastLogonDate -First 10
 
 #Locked Out accounts
-$LockedAccounts = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "OU=contoso Users,DC=internal,DC=contoso,DC=com" -Properties LockedOut | Where-Object {$_.LockedOut -match 'True'}
+$LockedAccounts = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "OU=NWMS Users,DC=internal,DC=contosoinc,DC=com" -Properties LockedOut | Where-Object {$_.LockedOut -match 'True'} | Select Name,SamAccountName,Title,Department,LastLogonDate,PasswordLastSet,LockedOut
 if(!$LockedAccounts){$LockedAccounts = 'None'}
 
 #Password Never expires
-$NeverExpires = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "OU=contoso Users,DC=internal,DC=contoso,DC=com" -Properties PasswordNeverExpires | Where-Object {$_.PasswordNeverExpires -match 'True'} | Select Name,SamAccountName,PasswordNeverExpires
+$NeverExpires = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "OU=NWMS Users,DC=internal,DC=contosoinc,DC=com" -Properties PasswordNeverExpires | Where-Object {$_.PasswordNeverExpires -match 'True'} | Select Name,SamAccountName,PasswordNeverExpires
 if(!$NeverExpires){$NeverExpires = 'None'}
 
 #Admin accounts
-$AdminAccounts = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "DC=internal,DC=contoso,DC=com" -Properties AdminCount | Where-Object {$_.AdminCount -gt 0} | Select Name,SamAccountName,AdminCount | Add-Member -MemberType ScriptProperty -Name Groups -Value {$((Get-ADPrincipalGroupMembership $this.SamAccountName).name)} -PassThru
+$AdminAccounts = Get-ADUser -Filter 'Enabled -eq $True' -SearchBase "DC=internal,DC=contosoinc,DC=com" -Properties AdminCount | Where-Object {$_.AdminCount -gt 0} | Select Name,SamAccountName,AdminCount | Add-Member -MemberType ScriptProperty -Name Groups -Value {$((Get-ADPrincipalGroupMembership $this.SamAccountName).name)} -PassThru
 
 $body ="
 <h1>AD User Report</h1>
@@ -74,4 +100,4 @@ $(ConvertHtml -intake $AdminAccounts -convertParams $convertParams)
 
 $to = "it@contoso.com"
 
-Send-Mailcontoso -body $body -to $to
+Send-Mailnwms -body $body -to $to -subject "Report-ADUser"
